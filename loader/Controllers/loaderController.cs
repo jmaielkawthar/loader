63564dcfca17f534366a0ec68f05ebca9477f578
Kawthar Jmaiel <k.jmaiel@root.sa>
2018-11-02 12:38:37 +0100

    Add project files.

---
+++ loader/Controllers/loaderController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace loader.Controllers
+{
+    public class loaderController : Controller
+    {
+        // GET: loader
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Submit()
+        {
+            Thread.Sleep(10000);
+            return Json("Submited Your Request");
+        }
+    }
+}
---
+++ loader/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="loader.MvcApplication" Language="C#" %>
