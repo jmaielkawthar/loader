bd5d69670417a676d24a2bbb12fc690a7ce62d44
Kawthar_Jmaiel <jmaielkawthar@gmail.com>
2019-08-19 13:23:43 +0300

    last in aout

---
+++ loader/Controllers/MenuSubMenuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace loader.Controllers
+{
+    public class MenuSubMenuController : Controller
+    {
+        // GET: MenuSubMenu
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
